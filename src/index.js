fd35a363740d4cd36cd62a7008e4483e1247ea65
Abhishek Lal <abhisheklalnediya@gmail.com>
2018-05-03 11:48:42 +0530

    changed port to read from env, and procfile

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { renderToString } from 'react-dom/server';
 import getTemperature from './utils';
 import Home from './home-page.jsx';
 
+const PORT = process.env.PORT || 3000
+
 const app = express();
 
 app.get('/api/temperature/:city', (req, res) => {
@@ -28,4 +30,4 @@ app.get('/', (req, res) => {
   res.send(renderToString(<Home />));
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
